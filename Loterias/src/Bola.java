ce1b192422a32c48daafe5d672ac9fb5cd2d804b
alexa <alexa@Phantom.home>
2017-12-30 13:44:40 -0200

    Início do projeto.

---
+++ Loterias/src/Bola.java
@@ -0,0 +1,18 @@
+
+public class Bola {
+	private int numero;
+	private boolean par;
+	private int coluna;
+	private int linha;
+	
+	public void setBola(int num, boolean pi, int col, int lin) {
+		this.numero = num;
+		this.par = pi;
+		this.coluna = col;
+		this.linha = lin;
+	}
+	
+	
+	
+	
+}
